130f73178d201c47a3743b11d774e1b0a8242ce3
natanaelwira <natanaelwira.u@gmail.com>
2025-11-12 21:52:53 +0700

    changes for admin menu access

--- src/main/java/com/example/wandoor/model/entity/MsMenu.java
+++ src/main/java/com/example/wandoor/model/entity/MsMenu.java
@@ -6,8 +6,6 @@ import jakarta.persistence.Id;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
-import java.sql.Clob;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -16,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(onlyExplicitlyIncluded = true)
-public class AdminMenuAccess {
+public class MsMenu {
     @Id
     @UuidGenerator
     @Column(nullable = false)
--- src/main/java/com/example/wandoor/service/AdminMenuAccessService.java
+++ src/main/java/com/example/wandoor/service/AdminMenuAccessService.java
@@ -1,6 +1,6 @@
 package com.example.wandoor.service;
 
-import com.example.wandoor.model.entity.AdminMenuAccess;
+import com.example.wandoor.model.entity.MsMenu;
 import com.example.wandoor.repository.AdminMenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,11 +15,11 @@ public class AdminMenuAccessService {
     private final AdminMenuRepository adminMenuRepository;
 
     public Map<String, Map<String, String>> getMenuStructure() {
-        List<AdminMenuAccess> allMenus = adminMenuRepository.findAll();
+        List<MsMenu> allMenus = adminMenuRepository.findAll();
         return allMenus.stream()
                 .collect(Collectors.groupingBy(
-                        AdminMenuAccess::getMenuName,
-                        Collectors.toMap(AdminMenuAccess::getMenuAction, AdminMenuAccess::getActionFlow)
+                        MsMenu::getMenuName,
+                        Collectors.toMap(MsMenu::getMenuAction, MsMenu::getActionFlow)
                 ));
     }
 }
