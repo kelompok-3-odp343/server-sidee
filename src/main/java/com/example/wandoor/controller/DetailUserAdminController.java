69ccfb44defb15872a1585278deca71eada9db87
natanaelwira <natanaelwira.u@gmail.com>
2025-11-12 20:34:35 +0700

    feature admin access menu

--- src/main/java/com/example/wandoor/controller/DetailUserAdminController.java
+++ src/main/java/com/example/wandoor/controller/DetailUserAdminController.java
@@ -2,10 +2,13 @@ package com.example.wandoor.controller;
 
 import com.example.wandoor.model.request.DetailUserAdminRequest;
 import com.example.wandoor.model.response.DetailUserAdminResponse;
+import com.example.wandoor.service.AdminMenuAccessService;
 import com.example.wandoor.service.DetailUserAdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -13,10 +16,20 @@ import org.springframework.web.bind.annotation.*;
 public class DetailUserAdminController {
 
     private final DetailUserAdminService detailUserAdminService;
+    private final AdminMenuAccessService adminMenuAccessService;
 
     @PostMapping("/detail-user")
     public ResponseEntity<DetailUserAdminResponse> getDetail(@RequestBody DetailUserAdminRequest request) {
         DetailUserAdminResponse response = detailUserAdminService.getUserDetail(request);
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/menu/access")
+    public ResponseEntity<Map<String, Map<String, String>>> getMenu(){
+        Map<String, Map<String, String>> response = adminMenuAccessService.getMenuStructure();
+        return ResponseEntity.ok(response);
+    }
+
+
+
 }
