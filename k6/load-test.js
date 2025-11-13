17e639e66f36885b8a6b76c2a42e05f5834742a2
RidwanSurya <ridwansuryaghani@gmail.com>
2025-11-13 11:02:27 +0700

    K6 Load Test Localhost 2

--- k6/load-test.js
+++ k6/load-test.js
@@ -7,7 +7,7 @@ export const options = {
 };
 
 export default function () {
-  const res = http.get('http://127.0.0.1:8081/api/v1/fetch-dashboard'); // ubah ke endpoint kamu
+  const res = http.get('http://127.0.0.1:8081/api/v1/fetch-dashboard'); // endpoint dashboard
   check(res, {
     'status 200': (r) => r.status === 200,
   });
