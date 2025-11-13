f92e482d82171e7bf73a8296e18ea0132a1fde01
RidwanSurya <ridwansuryaghani@gmail.com>
2025-11-13 10:42:55 +0700

    K6 Load Test Localhost

--- k6/load-test.js
+++ k6/load-test.js
@@ -2,12 +2,12 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 7,          // jumlah virtual user
+  vus: 50,          // jumlah virtual user
   duration: '30s',  // lama test
 };
 
 export default function () {
-  const res = http.get('http://localhost:8000/api/v1/fetch-dashboard'); // ubah ke endpoint kamu
+  const res = http.get('http://localhost:8081/api/v1/fetch-dashboard'); // ubah ke endpoint kamu
   check(res, {
     'status 200': (r) => r.status === 200,
   });
