c08eb4b7fd2792785602ecf8ad78af8fa3320504
Erlangga Wahyu Utomo <erlanggawahyu21@gmail.com>
2025-11-14 11:11:39 +0700

    Update conflict

--- src/main/java/com/example/wandoor/controller/AdminController.java
+++ src/main/java/com/example/wandoor/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.example.wandoor.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.wandoor.model.request.AdminApproverListRequest;
 import com.example.wandoor.model.request.AdminTransactionListRequest;
 import com.example.wandoor.model.request.DetailUserAdminRequest;
+import com.example.wandoor.model.response.AdminApproverDataResponse;
 import com.example.wandoor.model.response.AdminTransactionListResponse;
 import com.example.wandoor.model.response.DetailUserAdminResponse;
+import com.example.wandoor.model.response.GenericResponse;
+import com.example.wandoor.service.AdminApproverListService;
 import com.example.wandoor.service.AdminMenuAccessService;
 import com.example.wandoor.service.AdminTransactionListService;
 import com.example.wandoor.service.DetailUserAdminService;
@@ -27,6 +32,7 @@ public class AdminController {
     private final DetailUserAdminService detailUserAdminService;
     private final AdminTransactionListService service;
     private final AdminMenuAccessService adminMenuAccessService;
+    private final AdminApproverListService adminApproverListService;
 
     @PostMapping("/detail-user")
     public ResponseEntity<DetailUserAdminResponse> getDetail(@RequestBody DetailUserAdminRequest request) {
@@ -42,6 +48,14 @@ public class AdminController {
     @GetMapping("/menu/access")
     public ResponseEntity<Map<String, Map<String, String>>> getMenu(){
         Map<String, Map<String, String>> response = adminMenuAccessService.getMenuStructure();
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/approver-list")
+    public ResponseEntity<GenericResponse<List<AdminApproverDataResponse>>> getApproverList(
+            @RequestBody AdminApproverListRequest request
+            ) {
+        var response = adminApproverListService.getApproverList(request);
         return ResponseEntity.ok(response);
     }
 
