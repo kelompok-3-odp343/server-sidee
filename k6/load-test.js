ce1092ea25cd3e20ee568e9a11104a4694dc2d4b
RidwanSurya <ridwansuryaghani@gmail.com>
2025-11-24 09:07:54 +0700

    updating trivy

--- k6/load-test.js
+++ k6/load-test.js
@@ -2,72 +2,46 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 100,
-  duration: '100s',
+  vus: 1000,
+  duration: '10s',
 };
 
 export default function () {
   // Step 1: Login untuk trigger OTP
-  const loginRes = http.post(
-    'http://34.87.139.149:30080/api/auth/login',
-    JSON.stringify({
-      username: 'P006',  // akun test
-      password: '123456' // password test
-    }),
-    { headers: { 'Content-Type': 'application/json' } }
-  );
-
-  let loginJson;
-  try {
-    loginJson = JSON.parse(loginRes.body);
-  } catch {
-    console.error("Login response bukan JSON:", loginRes.body);
-    return;
-  }
-
-  // Ambil sessionId / otpId dari response login
-  const sessionId = loginJson.sessionIdOrToken || loginJson.otpId;
-  if (!sessionId) {
-    console.error("Login gagal atau OTP tidak dikirim:", loginRes.body);
-    return;
-  }
-  console.log("OTP ID / sessionId:", sessionId);
-
-  // Step 2: Verifikasi OTP (pakai OTP default test env)
-  // const otpRes = http.post('http://34.87.139.149:30080/api/auth/verify-otp',
+  // const loginRes = http.post(
+  //   'https://api.wandoor.my.id/api/auth/login',
   //   JSON.stringify({
-  //     sessionId: "f5ffca9b-ef09-4a8a-88a2-d621d91d83e3", 
-  //     otpCode: "637647" // OTP default di test environment
+  //     username: 'P004',  // akun test
+  //     password: '123456' // password test
   //   }),
   //   { headers: { 'Content-Type': 'application/json' } }
   // );
 
-  // let otpJson;
+  // let loginJson;
   // try {
-  //   otpJson = JSON.parse(otpRes.body);
+  //   loginJson = JSON.parse(loginRes.body);
   // } catch {
-  //   console.error("OTP response bukan JSON:", otpRes.body);
+  //   console.error("Login response bukan JSON:", loginRes.body);
   //   return;
   // }
 
-  // const token = otpJson.sessionIdOrToken;
-  // if (!token) {
-  //   console.error("Verifikasi OTP gagal:", otpRes.body);
+  // // Ambil sessionId / otpId dari response login
+  // const sessionId = loginJson.sessionIdOrToken || loginJson.otpId;
+  // if (!sessionId) {
+  //   console.error("Login gagal atau OTP tidak dikirim:", loginRes.body);
   //   return;
   // }
-  // console.log("Berhasil OTP → Token:", token);
+  // console.log("OTP ID / sessionId:", sessionId);
 
-  // // Step 3: Hit dashboard pakai token
-  // const dashRes = http.get(
-  //   'http://34.87.139.149:30080/api/v1/fetch-dashboard',
-  //   { headers: { Authorization: `Bearer ${token}` } }
-  // );
 
-  // if (dashRes.status !== 200) {
-  //   console.error("Dashboard error:", dashRes.status, dashRes.body);
-  // } else {
-  //   console.log("Dashboard berhasil:", dashRes.body);
-  // }
+  // Step 3: Hit dashboard pakai token
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJQMDAzIiwicm9sZSI6IkNIRUNLRVIiLCJlbWFpbCI6Im9rdGF2aWFxLmFAZ21haWwuY29tIiwibnBwIjoiNjQ4ODkiLCJpYXQiOjE3NjM1MjQ2OTUsImV4cCI6MTc2MzUyODI5NX0.zRb6SUuu45e1GyDlGjDOyhVWbhNgJq_ElYt_SpjKa44";
+
+  const dashRes = http.get(
+  'http://api.wandoor.my.id/api/admin/user/list',
+  { headers: { Authorization: `Bearer ${token}` } }
+  );
+
 
   sleep(1);
 }
